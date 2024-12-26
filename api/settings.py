fed828c1dc3d83624eaf07e86c00475eebf1e3ec
dharanish <dharanishwar.2006@gmail.com>
2024-12-25 12:20:17 -1200

    creating login page

--- api/settings.py
+++ api/settings.py
@@ -91,8 +91,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default': 
-        dj_database_url.parse(url=getenv("DATABASE_URL",'postgresql://neondb_owner:SVqg8TsNX1Ld@ep-tight-boat-a1l4btcv.ap-southeast-1.aws.neon.tech/e-comm?sslmode=require'),
-        conn_max_age = 600,conn_health_checks=True)
+        dj_database_url.parse(url=getenv("DATABASE_URL",'postgresql://neondb_owner:SVqg8TsNX1Ld@ep-tight-boat-a1l4btcv.ap-southeast-1.aws.neon.tech/e-comm?sslmode=require'),)
         # 'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': tmpPostgres.path.replace('/', ''),
         # 'USER': tmpPostgres.username,
