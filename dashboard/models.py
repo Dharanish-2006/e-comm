b9cb64f7e76a163885c1db4dffee9d7ec0a54a9c
dharanish <dharanishwar.2006@gmail.com>
2025-09-26 03:42:26 -1200

    dbchanges

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,21 +1,22 @@
 from django.db import models
-# from django.contrib.auth.models import User
 from authentication.models import User
 
 class Widget(models.Model):
     name = models.CharField(max_length=255)
-    widget_type = models.CharField(max_length=50)  # e.g., sales, inventory, etc.
-    content = models.TextField(default='Widget Content')  # Default content for the widget
+    widget_type = models.CharField(max_length=50)
+    content = models.TextField(default='Widget Content')
 
     def __str__(self):
         return self.name
 
+
 class Dashboard(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default='0')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     layout = models.JSONField(default=dict)
-    
+
     def __str__(self):
         return f"{self.user.username}'s Dashboard"
+
 
 class Alert(models.Model):
     type = models.CharField(max_length=255)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Dashboard, Widget
 
+@login_required
 def dashboard_view(request):
     dashboard, _ = Dashboard.objects.get_or_create(user=request.user)
-    Widgets = Widget.objects.all()
-    return render(request, 'dashboard.html', {'dashboard': dashboard, 'widgets': Widgets})
+    widgets = Widget.objects.all()
+    return render(request, 'dashboard.html', {
+        'dashboard': dashboard,
+        'widgets': widgets
+    })
